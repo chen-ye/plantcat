5b2e28c4aec8eb6daa5188d17fc777e4c2b1327e
ariellebryn <arielle.bryn@gmail.com>
2015-09-28 17:47:06 -0400

    Adding temp and LED

--- PhotoSensorDebug/PhotoSensorDebug.ino
+++ PhotoSensorDebug/PhotoSensorDebug.ino
@@ -12,24 +12,67 @@ Description: This sketch will make the arduino read Photo resistor
 ==============================================================
 */
 
-int photoRPin = 0; 
-int minLight;          //Used to calibrate the readings
-int maxLight;          //Used to calibrate the readings
-int lightLevel;
 elapsedMillis timeElapsed;
 
-int lightMin = 850; // Sunny: 900s, sunny shadows: 840
+// LED stuff
+int ledPin = 10;
+int ledOn = false;
+
+// Light Stuff
+int photoRPin = 0; 
+int lightLevel;
+
+int lightMin = 300; //850; // Sunny: 900s, sunny shadows: 840
+
+int lightAvg;
+
+// Temp Stuff
+int tempPin = 1;
+int temp;
+
+int tempMin = 0;
+int tempMax = 100;
+
 
 void setup() {
  Serial.begin(9600);
+ pinMode(ledPin, OUTPUT);
+ lightAvg = analogRead(photoRPin);
 }
 
 void loop(){
- //auto-adjust the minimum and maximum limits in real time
- lightLevel=analogRead(photoRPin);
+ // LIGHT READING
+ lightLevel = analogRead(photoRPin);
+
+ lightAvg = (lightAvg + lightLevel)/2;
+
+ //Serial.print("Current lightLevel: ");
+ //Serial.print(lightLevel);
+ //Serial.println();
  
  //Send the adjusted Light level result to Serial port (processing)
- Serial.println(lightLevel);
+ //Serial.println(lightAvg);
+
+
+ // TEMPERATURE READING 
+ temp = analogRead(tempPin);
+ 
+ // Convert to Celsius
+ float voltage = (temp * 5.0)/1023.0;
+ Serial.print(voltage); Serial.println(" volts");
+
+ float temperatureC = ((voltage*1000) - 500) / 10 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ //Serial.print(temperatureC); Serial.println(" degrees C");
+
+ // EMOTION
+ if (lightAvg >= lightMin && ledOn) {
+    digitalWrite(ledPin, LOW);
+    ledOn = false;
+ } else if (lightAvg < lightMin && !ledOn) {
+    digitalWrite(ledPin, HIGH);
+    ledOn = true;
+ }
  
  //slow down the transmission for effective Serial communication.
  delay(50);
